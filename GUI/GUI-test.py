26bda2b48f9619e9bd778b34f99f4f99e9eb928a
pershing9909 <820196796@qq.com>
2020-02-22 23:22:57 +0800

    update in 20200222

--- GUI/GUI-test.py
+++ GUI/GUI-test.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image
 import sys
 root= Tk()
 ###退出
@@ -21,19 +22,18 @@ sh = root.winfo_screenheight() #得到屏幕高度
 x = (sw-640) / 2
 y = (sh-360) / 2
 root.geometry('640x360+%d+%d' %(x,y))
-#win.geometry("%dx%d+%d+%d" %(ww,wh,x,y))
-                               #主体循环
+photo = PhotoImage(file="/Users/pershing9909/PycharmProjects/coreNet_stuff/GUI/ffdfds.gif")
+can=Canvas(root,width=129, height=45)
+can.place(x=10,y=3)
+can.create_image(65, 0, image=photo, anchor='n')
+welcomewords=Label(root,text='自研BRAS局数据检查工具',font=("黑体", 30, "bold"))
+welcomewords.place(x=140,y=12,width=400,height=30)
 
-top_welcome= Frame(root,height = 90,width = 320)
-top_welcome.pack(side=TOP,fill=BOTH)
+start_button=Button(None,text='开始',command=quit)
 
-#photo = PhotoImage(file="/Users/pershing9909/PycharmProjects/coreNet_stuff/2020pro/adjust_img.gif")
-#can=Canvas(top_welcome)
-#can.pack(fill=BOTH,side =LEFT)
-#can.create_image(2, 2, image=photo, anchor= W)
-Label(top_welcome,text='安徽移动自研BRAS局数据检查工具',bg='green').pack(side=LEFT)
-# start_button=Button(None,text='开始',command=quit)
-# quit_button=Button(None,text='退出',command=quit)
-# start_button.pack()
-# quit_button.pack()
-root.mainloop()
+quit_button=Button(None,text='退出',command=quit)
+
+
+
+
+root.mainloop()    #主体循环
